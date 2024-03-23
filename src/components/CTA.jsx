3cd16a907e615b25771a5b511b662bc43a8c47de
Lucifer7873 <freefireidcare@gmail.com>
2024-03-23 20:35:10 +0530

    commit

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,10 +6,10 @@ const CTA = () => {
 			<div className="container">
 				<div className="w-full text-center">
 					<div className="px-4">
-						<h2 className="mb-8 text-4xl font-semibold text-gray-800 lg:text-6xl">Pellentesque suscipit fringilla libero eu.</h2>
+						<h2 className="mb-8 text-4xl font-semibold text-gray-800 lg:text-6xl">Start a reliable and high-quality journey with our services.</h2>
 					</div>
 					<div className="px-4">
-						<Button text="Get a Demo" style="px-8 py-3.5 bg-green-500 text-white text-base font-medium" link="#" />
+						<Button text="Get in Touch" style="px-8 py-3.5 bg-green-500 text-white text-base font-medium" link="#" />
 					</div>
 				</div>
 			</div>
